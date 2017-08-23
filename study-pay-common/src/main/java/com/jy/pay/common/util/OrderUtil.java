19985d8f9fee83f2558a34d80bcb1865e2120596
Amen <362961910@qq.com>
2017-08-23 10:36:27 +0800

    完善OrderUtil

--- study-pay-common/src/main/java/com/jy/pay/common/util/OrderUtil.java
+++ study-pay-common/src/main/java/com/jy/pay/common/util/OrderUtil.java
@@ -4,10 +4,12 @@ package com.jy.pay.common.util;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.text.DecimalFormat;
 import java.util.Enumeration;
 
 public class OrderUtil {
 
+    public static  int seed = 0;
     private static String localIp = null;
 
     static {
@@ -39,8 +41,36 @@ public class OrderUtil {
         return ip.toString() ;
     }
 
+    private synchronized static int getSeed(){
+        if(++seed > 9999){
+            seed = 1;
+        }
+        return seed;
+    }
+
+    /**
+     * 生成唯一订单号。
+     *
+     * @param pre
+     * @return
+     */
+    private synchronized static String createOrderCode(long preOrder){
+        String pattern = "00";
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+        String patternSeed = "0000";
+        DecimalFormat seedFormat = new DecimalFormat(patternSeed);
+        if(null == localIp || "".equals(localIp)){
+            setLocalIp() ;
+        }
+        return decimalFormat.format(preOrder) + System.currentTimeMillis() + localIp + seedFormat.format(getSeed());
+    }
+
     public static void main(String[] args) {
         System.out.println("ip: " + localIp);
+
+        for (int i = 0; i < 20000; i++) {
+            System.out.println(createOrderCode(0));
+        }
     }
 
 }
